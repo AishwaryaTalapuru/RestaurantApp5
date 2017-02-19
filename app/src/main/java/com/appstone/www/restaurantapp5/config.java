0d4eed49b06278c1560fec7172dd73c395c82993
aishwaryatalapuru <aishwaryatalapuru@gmail.com>
2017-02-19 18:13:49 +0530

    initial commit

---
+++ app/src/main/java/com/appstone/www/restaurantapp5/config.java
@@ -0,0 +1,13 @@
+package com.appstone.www.restaurantapp5;
+
+/**
+ * Created by aishwaryatalapuru on 19/02/17.
+ */
+
+public class config {
+    public static final String DATA_URL = "http://192.168.94.1/Android/College/getData.php?id=";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_VC = "vc";
+    public static final String JSON_ARRAY = "result";
+}
